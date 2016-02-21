a357c6ed3d9bbe721244eaf6166ece5cd3d0f7be
plathir <panagiotis@panagiotis-work>
2016-02-21 15:47:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Upload files
+===========
+Yii-Framework extension for uploading files (Do not use ! under development... )
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist plathir/yii2-smart-upload "*"
+```
+
+or add
+
+```
+"plathir/yii2-smart-upload": "*"
+```
+
+to the require section of your `composer.json` file.
---
+++ actions/UploadAction.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace plathir\cropper\actions;
+
+use yii\base\Action;
+use yii\base\DynamicModel;
+use yii\base\InvalidConfigException;
+use yii\web\BadRequestHttpException;
+use yii\web\Response;
+use yii\web\UploadedFile;
+use plathir\cropper\Widget;
+use yii\imagine\Image;
+use Imagine\Image\Box;
+use Yii;
+
+class UploadAction extends Action {
+
+    public $temp_path;
+    public $uploadParam = 'file';
+    public $maxSize = 2097152;
+    public $extensions = 'jpeg, jpg, png, gif, zip, txt, doc';
+    public $multiple = false;
+
+    /**
+     * @inheritdoc
+     */
+    public function init() {
+        Widget::registerTranslations();
+        if ($this->temp_path === null) {
+            throw new InvalidConfigException(Yii::t('cropper', 'MISSING_ATTRIBUTE', ['attribute' => 'temp_path']));
+        } else {
+            $this->temp_path = rtrim(Yii::getAlias($this->temp_path), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function run() {
+        if (!$multiple) {
+            $this->uploadSingleFile();
+        } else {
+            $this->uploadMultipleFiles();
+        }
+    }
+
+    function uploadSingleFile() {
+        if (Yii::$app->request->isPost) {
+
+
+            $file = UploadedFile::getInstanceByName($this->uploadParam);
+            $model = new DynamicModel(compact($this->uploadParam));
+
+            $model->addRule($this->uploadParam, 'file', [
+                'maxSize' => $this->maxSize,
+                'tooBig' => Yii::t('cropper', 'TOO_BIG_ERROR', ['size' => $this->maxSize / (1024 * 1024)]),
+                'extensions' => explode(', ', $this->extensions),
+                'checkExtensionByMimeType' => false,
+                'wrongExtension' => Yii::t('cropper', 'EXTENSION_ERROR', ['formats' => $this->extensions])
+            ])->validate();
+
+            if ($model->hasErrors()) {
+                $result = [
+                    'error' => $model->getFirstError($this->uploadParam)
+                ];
+            } else {
+                $model->{$this->uploadParam}->name = uniqid() . '.' . $model->{$this->uploadParam}->extension;
+                if ($image->save($this->temp_path . $model->{$this->uploadParam}->name)) {
+                    $result = [
+                        'filelink' => $model->{$this->uploadParam}->name,
+                    ];
+                } else {
+                    $result = [
+                        'error' => Yii::t('cropper', 'ERROR_CAN_NOT_UPLOAD_FILE')]
+                    ;
+                }
+            }
+            Yii::$app->response->format = Response::FORMAT_JSON;
+
+            return $result;
+        } else {
+            throw new BadRequestHttpException(Yii::t('cropper', 'ONLY_POST_REQUEST'));
+        }
+    }
+
+    function uploadMultipleFiles() {
+        $files = UploadedFile::getInstancesByName($this->uploadParam);
+    }
+
+}
